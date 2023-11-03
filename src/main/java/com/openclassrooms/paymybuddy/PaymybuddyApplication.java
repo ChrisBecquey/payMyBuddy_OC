f1faa32b6397e6a8a442611c1ba31de29f20495d
ChrisBecquey <85166333+ChrisBecquey@users.noreply.github.com>
2023-11-03 15:09:04 +0100

    feat(clean): Clean import

--- src/main/java/com/openclassrooms/paymybuddy/PaymybuddyApplication.java
+++ src/main/java/com/openclassrooms/paymybuddy/PaymybuddyApplication.java
@@ -1,14 +1,7 @@
 package com.openclassrooms.paymybuddy;
 
-import com.openclassrooms.paymybuddy.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import com.openclassrooms.paymybuddy.service.UserService;
-
-import java.util.Optional;
 
 @SpringBootApplication
 public class PaymybuddyApplication{
